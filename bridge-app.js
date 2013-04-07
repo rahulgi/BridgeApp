26d8f53ef624cd70bbfe9900ec4a7376822a7413
Vivek Jain <viveksjain@gmail.com>
2013-04-06 17:53:27 -0700

    Add template to show list of indicators

--- bridge-app.js
+++ bridge-app.js
@@ -1,19 +1,37 @@
+Indicators = new Meteor.Collection("indicators");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to bridge-app.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  var tstrings = ["Concerning statements",
+                  "Behavioral cues",
+                  "Events/Situational cues",
+                  "Feelings"];
+  lstrings = [
+    ["Talking about suicide or discussing thoughts of suicide",
+        "Wish I were dead",
+        "Going to end it all",
+        "Won't be around much longer",
+        "Can't go on",
+        "Soon they won't have to worry about me"],
+     ["Has or looking for lethal means to kill oneself",
+        "Social withdrawal",
+        "Substance abuse",
+        "Not sleeping",
+        "Recklessness",
+        "Putting affairs in order",
+        "Rejecting help"],
+     ["Loss of someone close",
+        "Loss of academic opportunities",
+        "Fear of negative consequences",
+        "Depression"],
+     ["Hopelessness",
+        "Despair",
+        "Shame",
+        "Humiliation",
+        "Purposelessness"]];
+  Template.hello.helpers({
+    title: function () { return _.range(tstrings.length); },
+    tstring: function (t) { return tstrings[t]; },
+    listitem: function (t) { return _.range(lstrings[t].length); },
+    lstring: function (t, l) { return lstrings[t][l]; }
   });
 }
